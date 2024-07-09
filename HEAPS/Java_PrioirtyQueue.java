aba10a66645267f5f4786f1af2241d69c9f08ac2
Yash Raj Sharma <154724719+Yash1210codeing@users.noreply.github.com>
2024-07-09 20:20:50 +0530

    Add files via upload

---
+++ HEAPS/Java_PrioirtyQueue.java
@@ -0,0 +1,12 @@
+package HEAPS;
+import java.util.PriorityQueue;
+import java.util.Collections;
+//import java.util.Collection;
+public class Java_PrioirtyQueue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PriorityQueue<Integer>pq=new PriorityQueue<>();//min heap;
+		PriorityQueue<Integer>pq1=new PriorityQueue<>(Collections.reverseOrder());
+	}
+}
